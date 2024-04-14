24f18449cdd8cc9351a690c2beef0d544ecd5292
AddAcTech <aguilar.chavez.alexis.daniel@gmail.com>
2024-04-14 08:47:10 -0600

    welcome user

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 
 function Navigation() {
   const navigate = useNavigate();
+  const [user, setUser] = useState({});
   const handleNavigate = (goTo) => {
     if (goTo === "/") {
       localStorage.removeItem("user");
     }
     navigate(goTo);
   };
+  useEffect(() => {
+    const localUser = localStorage.getItem("user");
+    const userTo = JSON.parse(localUser);
+    setUser(userTo);
+  }, []);
+
   return (
-    <nav className="flex gap-10 items-center justify-between px-10 w-screen border-b py-2">
-      <img src={Logo} alt="Logo" width="125" />
+    <nav className="flex items-center justify-between px-10 w-screen border-b py-2 flex-wrap">
+      <div className="flex">
+        <img src={Logo} alt="Logo" width="125" />
+        <p className="text-2xl font-bold my-auto">Welcome {user.username}</p>
+      </div>
       <ul className="flex flex-wrap gap-1 justify-center">
         <li>
           <button

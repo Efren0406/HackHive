89f24269ae05eb94011df313d5235d735c8e9df7
Efren0406 <efrenchito477@gmail.com>
2024-04-14 08:38:52 -0600

    Adding chart to home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navigation from "./Navigation";
+import BarChart from "./chart";
 
 function Home() {
+  const data = {
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
+    values: [12, 19, 3, 5, 2, 3],
+  };
+
   return (
     <div>
       <Navigation />
       <h1 className="text-4xl text-center mt-8">Home</h1>
+      <BarChart data={data} />
     </div>
   );
 }
---
+++ client/src/components/chart.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const BarChart = ({ data }) => {
+  const chartRef = useRef(null);
+  let chartInstance = null;
+
+  useEffect(() => {
+    if (chartRef.current) {
+      if (chartInstance) {
+        chartInstance.destroy(); // Destruye el gráfico existente antes de inicializar uno nuevo
+      }
+
+      const ctx = chartRef.current.getContext('2d');
+      chartInstance = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: data.labels,
+          datasets: [{
+            label: 'Datos',
+            data: data.values,
+            backgroundColor: 'rgb(54,193,2, 0.2)',
+            borderColor: 'rgba(54,193,2, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+
+    return () => {
+      if (chartInstance) {
+        chartInstance.destroy(); // Limpia el gráfico al desmontar el componente
+      }
+    };
+  }, [data]);
+
+  return (
+    <div>
+      <canvas ref={chartRef}></canvas>
+    </div>
+  );
+};
+
+export default BarChart;

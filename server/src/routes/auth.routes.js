844bd5886ba44fdaec240083324a130546632a39
Efren0406 <efrenchito477@gmail.com>
2024-05-03 21:33:05 -0600

    Solo front-end

--- server/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { postUser, logUser } from "../controllers/auth.controller.js";
-
-const router = Router();
-
-router.post("/register", postUser);
-router.post("/login", logUser);
-
-export default router;
--- server/src/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { pong } from "../controllers/index.controller.js";
-
-const router = Router();
-
-router.get("/ping", pong);
-
-export default router;
